df708a1b887c1d1a2352f7f0887e1559e7b231dd
Alin Voinea <contact@avoinea.com>
2023-04-10 19:11:06 +0300

    refactor(Cleanup): Drop Python2/Plone4 support

--- eea/volto/policy/interfaces.py
+++ eea/volto/policy/interfaces.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
-from eea.volto.policy import EEAMessageFactory as _
-from zope import schema
-from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IEeaVoltoPolicyLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
-
-
-class IVoltoSettings(Interface):
-    """Volto settings necessary to store in the backend"""
-
-    frontend_domain = schema.URI(
-        title="Frontend domain",
-        description=_("Used for rewriting URL's sent in the password reset "
-                      "e-mail by Plone."),
-        default="http://localhost:3000",
-    )
