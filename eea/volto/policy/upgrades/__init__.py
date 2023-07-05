a1004f2e535eaa394e4dcaeefa16782f079579ba
nileshgulia1 <nileshgulia@gmail.com>
2023-07-05 14:20:32 +0530

    upgrade step for catalog

--- eea/volto/policy/upgrades/__init__.py
+++ eea/volto/policy/upgrades/__init__.py
@@ -1,2 +1,9 @@
 """ Upgrades
 """
+from plone import api
+
+
+def upgrade_catalog(context):
+    portal_setup = api.portal.get_tool("portal_setup")
+    portal_setup.runImportStepFromProfile(
+        "eea.volto.policy:volto_policy__23", "catalog")
