27c704c47fd6dab953b1f5c917b22abf39060820
nileshgulia1 <nileshgulia@gmail.com>
2023-07-05 14:46:22 +0530

    pylint: docstring

--- eea/volto/policy/upgrades/__init__.py
+++ eea/volto/policy/upgrades/__init__.py
@@ -4,6 +4,8 @@ from plone import api
 
 
 def upgrade_catalog(context):
+    """upgrade portal catalog."""
+
     portal_setup = api.portal.get_tool("portal_setup")
     portal_setup.runImportStepFromProfile(
         "eea.volto.policy:volto_policy__23", "catalog")
