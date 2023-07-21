2f6953dc7c5dd9c5e99e7eeba2421d3acf98554c
Alin Voinea <contact@avoinea.com>
2023-07-21 10:44:16 +0300

    chore: Cleanup unneeded separate upgrade step - refs #254889

--- eea/volto/policy/upgrades/__init__.py
+++ eea/volto/policy/upgrades/__init__.py
@@ -1,11 +1,2 @@
 """ Upgrades
 """
-from plone import api
-
-
-def upgrade_catalog(context):
-    """upgrade portal catalog."""
-
-    portal_setup = api.portal.get_tool("portal_setup")
-    portal_setup.runImportStepFromProfile(
-        "eea.volto.policy:volto_policy__23", "catalog")
