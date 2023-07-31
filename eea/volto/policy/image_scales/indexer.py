c1b08281d86d5ba32451e1804b2d1ef6ac562083
nileshgulia1 <nileshgulia@gmail.com>
2023-07-31 16:11:28 +0530

    add: patch serializer for ImageScaleFields

--- eea/volto/policy/image_scales/indexer.py
+++ eea/volto/policy/image_scales/indexer.py
@@ -3,11 +3,17 @@ Indexer
 """
 
 from persistent.dict import PersistentDict
-from plone.base.interfaces import IImageScalesAdapter
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer.decorator import indexer
 from zope.component import queryMultiAdapter
 from zope.globalrequest import getRequest
+from zope.interface import Interface
+
+try:
+    from plone.base.interfaces import IImageScalesAdapter
+except ImportError:
+    # BBB Plone 5
+    IImageScalesAdapter = Interface
 
 
 @indexer(IDexterityContent)
@@ -20,7 +26,10 @@ def image_scales(obj):
         # Raising an AttributeError does the right thing,
         # making sure nothing is saved in the catalog.
         raise AttributeError
-    scales = adapter()
+    try:
+        scales = adapter()
+    except TypeError:
+        scales = {}
     if not scales:
         raise AttributeError
     return PersistentDict(scales)
