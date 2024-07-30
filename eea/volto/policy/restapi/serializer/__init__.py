98fd671b478c96240d961d410c2d49ab508ba08c
David Ichim <ichim.david@gmail.com>
2024-07-30 21:18:29 +0300

    Add registry entry for side_nav_types

--- eea/volto/policy/restapi/serializer/__init__.py
+++ eea/volto/policy/restapi/serializer/__init__.py
@@ -3,6 +3,11 @@
 from plone.app.layout.navigation.root import getNavigationRoot
 from plone.restapi.services.contextnavigation import get as original_get
 
+from plone.restapi import bbb as restapi_bbb
+from plone import schema
+
+from plone.base import PloneMessageFactory as _
+
 
 class EEAContextNavigationQueryBuilder(original_get.QueryBuilder):
     """ Custom QueryBuilder for context navigation
@@ -29,5 +34,23 @@ class EEAContextNavigationQueryBuilder(original_get.QueryBuilder):
             # EEA modification to use bottomLevel for depth of navtree_start
             self.query["path"]["navtree_start"] = depth + 1  # 4
 
-# Apply the patch
 original_get.QueryBuilder = EEAContextNavigationQueryBuilder
+
+
+class IEEAContextNavigationSchema(restapi_bbb.INavigationSchema):
+    """ Custom schema for context navigation
+    """
+    side_nav_types = schema.Tuple(
+        title=_("Displayed content types within the side navigation"),
+        description=_(
+            "The content types that should be shown in the side navigation"
+        ),
+        required=False,
+        default=("Document"),
+        missing_value=(),
+        value_type=schema.Choice(
+            source="plone.app.vocabularies.ReallyUserFriendlyTypes"
+        ),
+    )
+
+restapi_bbb.INavigationSchema = IEEAContextNavigationSchema
