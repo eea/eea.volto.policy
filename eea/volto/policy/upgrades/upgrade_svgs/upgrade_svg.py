3aa5cf410435983199137626e262db11e96f01a0
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-10-09 14:50:00 +0300

    Update upgrade_svg.py

--- eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
+++ eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
@@ -7,25 +7,35 @@ from plone.restapi.interfaces import ISerializeToJson
 from zope.component import getMultiAdapter
 from plone.namedfile.utils import getImageInfo
 from zope.lifecycleevent import modified
+
 logger = logging.getLogger("upgrade_svgs")
 logger.setLevel(logging.INFO)
 
+
 def upgrade_svgs(portal):
-    """Upgrade all svgs to fix the width and height property"""
+    """Upgrade all teaserGrid blocks to gridBlock and log SVG dimensions"""
     i = 0
     for brain in portal.portal_catalog():
         obj = brain.getObject()
-        if hasattr(obj, 'image') and hasattr(obj.image, '_width') and hasattr(obj.image, '_height'):
+        if (
+            hasattr(obj, "image")
+            and hasattr(obj.image, "_width")
+            and hasattr(obj.image, "_height")
+        ):
             logger.info("Processing %s", obj.absolute_url())
-            contentType,width,height = getImageInfo(obj.image.data)
+            contentType, width, height = getImageInfo(obj.image.data)
             if contentType == "image/svg+xml":
                 obj.image._width = width
                 obj.image._height = height
                 modified(obj.image)
                 i += 1
-        if hasattr(obj, 'preview_image') and hasattr(obj.preview_image, '_width') and hasattr(obj.preview_image, '_height'):
+        if (
+            hasattr(obj, "preview_image")
+            and hasattr(obj.preview_image, "_width")
+            and hasattr(obj.preview_image, "_height")
+        ):
             logger.info("Processing %s", obj.absolute_url())
-            contentType,width,height = getImageInfo(obj.preview_image.data)
+            contentType, width, height = getImageInfo(obj.preview_image.data)
             if contentType == "image/svg+xml":
                 obj.preview_image._width = width
                 obj.preview_image._height = height
@@ -34,4 +44,4 @@ def upgrade_svgs(portal):
         if not i % 100:
             logger.info(i)
             transaction.commit()
-    transaction.commit() 
+    transaction.commit()
