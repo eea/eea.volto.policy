9df8c8599aa3465fa7626b03dee14df70f61fcad
Teodor <teodorvoicu87@gmail.com>
2025-04-16 15:14:25 +0300

    adapter for serializer in creators

--- eea/volto/policy/restapi/serializer/dxfields.py
+++ eea/volto/policy/restapi/serializer/dxfields.py
@@ -1,5 +1,6 @@
 """ DXFields
 """
+from plone import api
 from plone.app.dexterity.behaviors.metadata import IPublication
 from plone.dexterity.interfaces import IDexterityContent
 from plone.restapi.interfaces import IFieldSerializer
@@ -9,7 +10,16 @@ from plone.namedfile.interfaces import INamedFileField
 from zope.component import adapter
 from zope.interface import implementer
 from zope.schema.interfaces import IDatetime
-
+from plone.app.dexterity.behaviors.metadata import IOwnership
+from zope.schema.interfaces import ITuple
+from zope.publisher.interfaces.browser import IBrowserRequest
+from plone.dexterity.interfaces import IDexterityContent
+from plone.restapi.interfaces import IFieldDeserializer
+from plone.restapi.serializer.converters import json_compatible
+from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+from zope.component.hooks import getSite
+import copy
 
 from eea.volto.policy.interfaces import IEeaVoltoPolicyLayer
 try:
@@ -18,6 +28,28 @@ except ImportError:
     # Fallback
     ICoreMetadata = IPublication
 
+
+@implementer(IFieldSerializer)
+@adapter(ITuple, IDexterityContent, Interface)
+class CreatorsFieldSerializer(DefaultFieldSerializer):
+    """Creators field serializer"""
+
+    def __call__(self):
+        value = copy.deepcopy(self.get_value())
+        print('first value', value)
+
+        if self.field is IOwnership["creators"]:
+            fullnames = []
+            for userid in value:
+                user = api.user.get(userid)
+                if user:
+                    fullname = user.getProperty("fullname", "")
+                    fullnames.append(fullname if fullname else userid)
+                else:
+                    fullnames.append(userid)
+
+            value = fullnames
+        return json_compatible(value)
 
 @adapter(IDatetime, IDexterityContent, IEeaVoltoPolicyLayer)
 @implementer(IFieldSerializer)
