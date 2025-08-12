b1967d28097bcf8dfddd56f6de56e080b480ab57
narcis2005 <chirilov.narcis@yahoo.ro>
2025-08-12 17:19:38 +0300

    Make upgrade step work

--- eea/volto/policy/upgrades/internal_api_path.py
+++ eea/volto/policy/upgrades/internal_api_path.py
@@ -1,10 +1,8 @@
 """Upgrade step to add registries"""
-from plone import api
-
-PROFILE_ID = "profile-eea.volto.policy:default"
 
 
 def add_internal_api_path_registry(context):
-    """Add IInternalApiPathSettings registry record."""
-    setup = api.portal.get_tool("portal_setup")
-    setup.runImportStepFromProfile(PROFILE_ID, "plone.app.registry")
+    """Upgrade step to import the control panel and registry configs."""
+    profile_id = 'profile-eea.volto.policy:default'
+    context.runImportStepFromProfile(profile_id, 'controlpanel')
+    context.runImportStepFromProfile(profile_id, 'plone.app.registry')
