e790f57b5000a8a3040b7a16ba74ff69e1c50137
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-08-19 13:06:41 +0300

    implement feedback

--- eea/volto/policy/upgrades/internal_api_path.py
+++
@@ -1,8 +0,0 @@
-"""Upgrade step to add registries"""
-
-
-def add_internal_api_path_registry(context):
-    """Upgrade step to import the control panel and registry configs."""
-    profile_id = 'profile-eea.volto.policy:default'
-    context.runImportStepFromProfile(profile_id, 'controlpanel')
-    context.runImportStepFromProfile(profile_id, 'plone.app.registry')
