a3c85f75e674c30fe69787a350ce784a8687a6bf
David Ichim <ichim.david@gmail.com>
2025-10-06 21:56:20 +0300

    fix(teaser): add TeaserBlockSerializerBase monkey patch to fix  empty href
    - When we have an external app such as the Data Hub which is served within the eea website, the url ends up relative and catalog search cannot find any datahub entries.
      See https://github.com/plone/plone.restapi/blob/main/src/plone/restapi/serializer/blocks.py#L254 for the original code

--- eea/volto/policy/restapi/serializer/__init__.py
+++ eea/volto/policy/restapi/serializer/__init__.py
@@ -1,1 +1,10 @@
-"""RestAPI serializer"""
+"""RestAPI serializer
+
+This module applies monkey patches to plone.restapi.serializer.blocks
+"""
+
+# Import monkey patches for plone.restapi.serializer.blocks
+# This will apply the TeaserBlockSerializerBase._process_data patch
+from .blocks import apply_teaser_block_monkey_patch
+
+apply_teaser_block_monkey_patch()
